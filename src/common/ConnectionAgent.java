a5a5524d952d607456209789f6771501c7b6f2f6
katherineb913 <94199642+katherineb913@users.noreply.github.com>
2021-11-12 20:09:02 -0500

    skeleton update

--- src/common/ConnectionAgent.java
+++ src/common/ConnectionAgent.java
@@ -1,4 +1,41 @@
 package common;
 
-public class ConnectionAgent {
+import java.io.PrintStream;
+import java.net.Socket;
+import java.util.Scanner;
+
+/**
+ * This class represents the connection that is being made between the client and server.
+ *
+ * @author Brett Dale
+ * @author Katherine Blanton
+ */
+public class ConnectionAgent implements Runnable{
+
+    private Socket socket;
+    private Scanner in;
+    private PrintStream out;
+    private Thread thread;
+
+    public ConnectionAgent(Socket socket){
+
+    }
+
+    public void sendMessage(String message){
+
+    }
+
+    public boolean isConnected(){
+        boolean connected = false;
+        return connected;
+    }
+
+    public void close(){
+
+    }
+
+    @Override
+    public void run() {
+
+    }
 }
