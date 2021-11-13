e05de85e8200e00ac3d71e0bd3e0f5972e26c71b
katherineb913 <94199642+katherineb913@users.noreply.github.com>
2021-11-12 20:10:18 -0500

    skeleton update

--- src/server/BattleServer.java
+++ src/server/BattleServer.java
@@ -1,4 +1,40 @@
 package server;
 
-public class BattleServer {
+import common.MessageListener;
+import common.MessageSource;
+import java.net.ServerSocket;
+
+/**
+ * This class represents the server for where the client will communicate with the client about the game.
+ *
+ * @author Brett Dale
+ * @author Katherine Blanton
+ */
+public class BattleServer implements MessageListener {
+
+    private ServerSocket serverSocket;
+    private int current;
+    private Game game;
+
+    public BattleServer(int port){
+
+    }
+
+    public void listen(){
+
+    }
+
+    public void broadcast(String message){
+
+    }
+
+    @Override
+    public void messageReceived(String message, MessageSource source) {
+
+    }
+
+    @Override
+    public void sourceClosed(MessageSource source) {
+
+    }
 }
