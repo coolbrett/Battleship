3a6e7028dca34c23fb5b0cbd44e8468107f08b9a
katherineb913 <94199642+katherineb913@users.noreply.github.com>
2021-12-08 21:11:10 -0500

    Updated a few methods
    Not for sure how everything works but added a few fields I thought we might need

--- src/server/BattleServer.java
+++ src/server/BattleServer.java
@@ -1,8 +1,12 @@
 package server;
 
+import common.ConnectionAgent;
 import common.MessageListener;
 import common.MessageSource;
+
+import java.io.*;
 import java.net.ServerSocket;
+import java.net.Socket;
 
 /**
  * This class represents the server for where the client will communicate with the client about the game.
@@ -16,12 +20,41 @@ public class BattleServer implements MessageListener {
     private int current;
     private Game game;
 
-    public BattleServer(int port){
+    private ConnectionAgent[] ConArray;
+    private int port;
 
+    public BattleServer(int port) throws IOException {
+        this.port = port;
     }
 
     public void listen(){
+        try {
+            // Create a TCP socket to send data through
+            ServerSocket server = new ServerSocket(this.port);
 
+            // While the server socket is not closed
+            while (!server.isClosed()) {
+                // Accept an incoming connection
+                Socket sock = server.accept();
+
+                // Create readers to read data being sent by client
+                InputStreamReader inStream = new InputStreamReader(sock.getInputStream());
+                BufferedReader buffer = new BufferedReader(inStream);
+
+                // Get the socket output stream and wrap it to write objects
+                OutputStream sockOut = sock.getOutputStream();
+                ObjectOutputStream outStream = new ObjectOutputStream(sockOut);
+
+                // Close streams for this connection
+                sock.close();
+                inStream.close();
+                buffer.close();
+                sockOut.close();
+                outStream.close();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public void broadcast(String message){
@@ -35,6 +68,7 @@ public class BattleServer implements MessageListener {
 
     @Override
     public void sourceClosed(MessageSource source) {
+        source.removeMessageListener(this);
 
     }
 }
